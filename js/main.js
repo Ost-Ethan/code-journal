eaf16d7568f189ed4dbcd9ac472f6f1f4fc24099
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-09-19 17:03:38 +0000

    created function renderEntry and a test entry to ensure proper functionality, commented created dom tree

--- js/main.js
+++ js/main.js
@@ -31,3 +31,62 @@ function addButtonHandler(event) {
   $journalForm.reset();
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
+
+function renderEntry(entry) {
+  const $divRowListItem = document.createElement('div');
+  const $divColumnFullListItem = document.createElement('div');
+  const $ulEntry = document.createElement('ul');
+  const $liEntry = document.createElement('li');
+  const $imgEntryImage = document.createElement('img');
+  const $divTitleList = document.createElement('div');
+  const $h2EntryTitle = document.createElement('h2');
+  const $divNotesEntry = document.createElement('div');
+  const $pNotes = document.createElement('p');
+
+  $divRowListItem.className = 'row';
+  $divColumnFullListItem.className = 'column-full';
+  $imgEntryImage.className = 'image';
+  $imgEntryImage.setAttribute('src', entry.photoUrl);
+  $h2EntryTitle.textContent = entry.title;
+  $pNotes.textContent = entry.notes;
+
+  $divRowListItem.appendChild($divColumnFullListItem);
+  $divColumnFullListItem.appendChild($ulEntry);
+  $ulEntry.appendChild($liEntry);
+  $liEntry.appendChild($imgEntryImage);
+  $liEntry.appendChild($divTitleList);
+  $liEntry.appendChild($divNotesEntry);
+  $divTitleList.appendChild($h2EntryTitle);
+  $divNotesEntry.appendChild($pNotes);
+
+  /*  This function creates the following dom tree in which a journal entry's properties are displayed:
+          <div class="row">
+          <div class="column-full">
+            <ul>
+              <li>
+                <img
+                  class="image"
+                  src=""
+                  alt="" />
+                <div>
+                  <h2>title goes here</h2>
+                </div>
+                <div>
+                  <p>Notes go here</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        */
+  // console.log("DOM Tree template with list entry:", $divRowListItem);
+}
+
+const testEntry = {
+  title: 'Burger',
+  photoUrl: 'BurgerURL',
+  notes: 'Notes go here',
+  entryID: 1,
+};
+
+renderEntry(testEntry);
